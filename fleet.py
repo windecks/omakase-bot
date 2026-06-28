386e5e3321343e6a9049d99800bd9ceb14e1aec1
windex <JOSEPH1231123104@gmail.com>
2026-06-28 15:39:02 -0400

    bug

--- fleet.py
+++ fleet.py
@@ -131,6 +131,7 @@ def main():
                             futures[executor.submit(run_task, task_dict, 0)] = task_dict
             except concurrent.futures.TimeoutError:
                 # Expected timeout every 60s if no tasks complete, allows loop to re-evaluate the update timer
+                pass
     
         logging.info("All tasks completed successfully. Idling to prevent Docker from restarting the container...")
         while True:
